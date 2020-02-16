ee324e9bd01b15d9288ec5d7854bf47b5f816993
ziyadnz <58900772+ziyadnz@users.noreply.github.com>
2020-02-16 22:27:16 +0300

    final

--- person.java
+++ person.java
@@ -1,58 +1,41 @@
 package hw1;
 
-import java.util.Arrays;
-import java.util.Random;
-
 public class person {
 
-	public person() {
+	String name;
+	int health;
+	int roomNumber;
+	
 
+	public person(String name, int health, int roomNumber) {
+		super();
+		this.name = name;
+		this.health = health;
+		this.roomNumber = roomNumber;
 	}
 
-	public static void hide(int number3) {
-
-		// saklanma yeri mom
-		int[][] box = new int[5][5];
-		for (int i = 0; i < box.length; i++) {
-			for (int j = 0; j < box.length; j++) {
-				if (i == number3) {
-					box[i][j] = 1;
-				} else
-					box[i][j] = 0;
-			}
-		}
+	public String getName() {
+		return name;
 	}
 
-	public static void hide(int number1, int number2) {
-		// saklanma yeri girl
-		int[][] box = new int[5][5];
-		for (int i = 0; i < box.length; i++) {
-			for (int j = 0; j < box.length; j++) {
-				box[i][j] = 0;
-			}
-		}
-		box[number1][number2] = 1;
+	public void setName(String name) {
+		this.name = name;
 	}
 
-	public static void father(int hp) {
-		int health = 100;
-		int[][] find = new int[5][5];
-		for (int i = 0; i < find.length; i++) {
-			for (int j = 0; j < find.length; j++) {
-				find[i][j] = 1;
-				health = health - hp;
-
-			}
-		}
-
+	public int getHealth() {
+		return health;
 	}
 
-	public static void mother(int number3) {
-		hide(number3);
+	public void setHealth(int health) {
+		this.health = health;
 	}
 
-	public static void doughter(int number1, int number2) {
-		hide(number1, number2);
+	public int getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(int roomNumber) {
+		this.roomNumber = roomNumber;
 	}
 
 }
